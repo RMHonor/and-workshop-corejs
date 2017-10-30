f08dd7f0f4ad7cb2b52f6a4f05c9f41e736881c3
Richard Honor <me@r-honor.uk>
2017-10-30 10:17:54 +0000

    Fixed stuff

--- 01_variables.test.js
+++ 01_variables.test.js
@@ -1,18 +1,17 @@
 test('VAR', () => {
   var x = 5;
-  var x = 6;
   expect(x).toBe(5);
 });
 
 test('LET and VAR', () => {
   var x = 6;
-  let x = 5;
+  x = 5;
   expect(x).toBe(5);
 });
 
 test('LET', () => {
   let x = 5;
-  let x = 6;
+  x = 6;
   expect(x).toBe(6);
 });
 
@@ -20,23 +19,23 @@ test('LET', () => {
   let x = 5;
 
   function foo() {
-    let x = 20;
+    x = 20;
 
     return x;
   }
+
+  x = foo();
 
   expect(x).toBe(20);
 });
 
 test('CONST - scalar values', () => {
   const x = 5;
-  x = 'foo';
   expect(x).toBe(5);
 });
 
 test('CONST - assignment', () => {
-  const x;
-  x = 5
+  const x = 5
   expect(x).toBe(5);
 });
 
@@ -45,6 +44,8 @@ test('CONST - objects', () => {
     "name": "Linus",
     "age": 42
   };
-  
+
+  person.lastname = 'torvalds';
+
   expect(person.lastname).toBe('torvalds');
 });
