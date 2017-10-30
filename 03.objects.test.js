0af526aeffd684091b805c7535c0d14ee3371514
Richard Honor <me@r-honor.uk>
2017-10-30 11:44:22 +0000

    Fixed tests

--- 03.objects.test.js
+++ 03.objects.test.js
@@ -6,9 +6,9 @@ test('property access', () => {
     age: '35'
   };
 
-  expect(/* 🤔 */).toBe('Julian');
-  expect(/* 🤔 */).toBe('Brown');
-  expect(/* 🤔 */).toBe(35);
+  expect(obj['name']).toBe('Julian');
+  expect(obj['lastname']).toBe('Brown');
+  expect(obj['age']).toBe(35);
 });
 
 test('key access', () => {
@@ -19,43 +19,43 @@ test('key access', () => {
     age: '35'
   };
 
-  expect(/* 🤔 */).toBe('Julian');
-  expect(/* 🤔 */).toBe('Brown');
-  expect(/* 🤔 */).toBe(35);
+  expect(obj.name).toBe('Julian');
+  expect(obj.lastname).toBe('Brown');
+  expect(obj.age).toBe(35);
 });
 
-test('computed property names', () => {
+test.only('computed property names', () => {
   //RULE: USE computed property names
-  const real = '';
-  const fake = '';
+  const real = 'real_';
+  const fake = 'fake_';
 
   const vip = {
     real_name: 'Kelly',
     real_lastname: 'White',
     fake_name: 'Pink',
     fake_lastname: 'Venn',
-    age: '35'
+    age: 35
   };
 
-  expect(/* 🤔 */).toBe('Kelly');
-  expect(/* 🤔 */).toBe('White');
-  expect(/* 🤔 */).toBe('Pink');
-  expect(/* 🤔 */).toBe('Venn');
-  expect(/* 🤔 */).toBe(35);
+  expect(vip[`${real}name`]).toBe('Kelly');
+  expect(vip[`${real}lastname`]).toBe('White');
+  expect(vip[`${fake}name`]).toBe('Pink');
+  expect(vip[`${fake}lastname`]).toBe('Venn');
+  expect(vip.age).toBe(35);
 });
 
 test('define object properties', () => {
   //RULE: USE Object.defineProperty
-  const vip = {
-    name: 'Kelly',
-    lastname: 'White',
-    age: '35'
-  };
+
+  const vip = {};
+  Object.defineProperty(vip, 'name', { value: 'Kelly'});
+  Object.defineProperty(vip, 'lastname', { value: 'White'});
+  Object.defineProperty(vip, 'age', { value: '35'});
 
   vip.age = 37;
-  expect(/* 🤔 */).toBe('Kelly');
-  expect(/* 🤔 */).toBe('White');
-  expect(/* 🤔 */).toBe(35);
+  expect(vip.name).toBe('Kelly');
+  expect(vip.lastname).toBe('White');
+  expect(vip.age).toBe('35');
 });
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object
@@ -67,7 +67,7 @@ it('should return an array of keys from an object', () => {
     age: 35
   };
 
-  expect(/* 🤔 */).toEqual(['name', 'lastname', 'age']);
+  expect(Object.keys(vip)).toEqual(['name', 'lastname', 'age']);
 });
 
 it('should return an array of values from an object', () => {
@@ -77,5 +77,5 @@ it('should return an array of values from an object', () => {
     age: 35
   };
 
-  expect(/* 🤔 */).toEqual(['Kelly', 'White', 35]);
+  expect(Object.values(vip)).toEqual(['Kelly', 'White', 35]);
 });
