ade430b5fc4243cd7b301fb27cc62ec3faa57b92
Richard Honor <me@r-honor.uk>
2017-10-31 11:02:25 +0000

    Fixed this tests

--- 11_this.test.js
+++ 11_this.test.js
@@ -5,46 +5,47 @@ it('should print the name of the person objects', () => {
 
   const john = { name: 'John' };
 
-  expect(/* 🤔 */).toBe('John'); // USE bind https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Function/bind
-  expect(/* 🤔 */).toEqual('John'); // USE call https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/call
-  expect(/* 🤔 */).toEqual('John'); // USE apply https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/apply
+  const getNameBound = getName.bind(john);
+
+  expect(getNameBound()).toBe('John'); // USE bind https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Function/bind
+  expect(getName.call(john)).toEqual('John'); // USE call https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/call
+  expect(getName.apply(john)).toEqual('John'); // USE apply https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/apply
 });
 
 it('should print the name of the person objects', () => {
-  function Person() {
-    /* ... */
+  function Person(name, age, isFine) {
+    this.name = name;
+    this.age = age;
+    this.isFine = () => isFine;
   }
 
-  const john = null;
+  const john = new Person('John', 28, true);
 
   expect(john.age).toBe(28);
   expect(john.name).toEqual('John');
   expect(john.isFine()).toBe(true);
 });
 
-it('should return the maximu number in an array', () => {
+it('should return the maximum number in an array', () => {
   //don't google it, try it first! hint: use apply and https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max
   const numbers = [1, 99, 34, 1000, 123];
 
-  expect(/* 🤔 */).toBe(1000);
+  expect(Math.max.apply(this, numbers)).toBe(1000);
 });
 
-it('should return the maximu number in an array', () => {
-  const leaderBoard = {
-    scores: [900, 845, 809, 950],
-    avgScore: null,
-    avg: function() {
-      let sumOfScores = this.scores.reduce(function(prev, cur, index, array) {
-        return prev + cur;
-      });
-      this.avgScore = sumOfScores / this.scores.length;
-    }
+it('should return the average number in an array', () => {
+  function LeaderBoard(scores) {
+    this.scores = scores;
+  }
+
+  LeaderBoard.prototype.avg = function() {
+    const sumOfScores = this.scores.reduce((prev, cur) => prev + cur);
+    this.avgScore = sumOfScores / this.scores.length;
+
+    return this;
   };
 
-  const anotherleaderBoard = {
-    scores: [8, 10, 8, 9, 10, 9],
-    avgScore: null
-  };
+  const anotherleaderBoard = new LeaderBoard([8, 10, 8, 9, 10, 9]);
 
-  expect(anotherleaderBoard.avgScore).toBe(9);
+  expect(anotherleaderBoard.avg().avgScore).toBe(9);
 });
